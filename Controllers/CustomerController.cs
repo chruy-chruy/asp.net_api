37045ada9f73a875becc2aa013a215e5686a8192
Yourname <you@example.com>
2024-05-08 23:12:44 +0800

    first commit

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,92 @@
+﻿// Controllers/CustomerController.cs
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly SampleDBContext _context;
+        public CustomerController(SampleDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customer
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetCustomers()
+        {
+            return _context.Customer.ToList();
+        }
+
+        // GET: api/Customer/1
+        [HttpGet("{id}")]
+        public ActionResult<Customer> GetCustomer(int id)
+        {
+            var customer = _context.Customer.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return customer;
+        }
+
+        // POST: api/Customer
+        [HttpPost]
+        public ActionResult<Customer> CreateCustomer(Customer customer)
+        {
+            if (customer == null)
+            {
+                return BadRequest();
+            }
+            _context.Customer.Add(customer);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetCustomer), new { id = customer.CustomerId }, customer);
+        }
+
+        // Put: api/Customer/id
+        [HttpPut("{id}")]
+        public ActionResult<Customer> Update(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return BadRequest();
+            }
+
+            var existing = _context.Customer.Find(id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+
+            _context.Entry(existing).State = EntityState.Detached;
+            _context.Customer.Update(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // Delete: api/Customer/1
+        [HttpDelete("{id}")]
+        public ActionResult<Customer> Delete(int id)
+        {
+            var customer = _context.Customer.Find(id);
+            if (customer == null)
+            {
+                return BadRequest();
+            }
+
+
+            _context.Customer.Remove(customer);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            //This section below is for connection string 
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<SampleDBContext>(options => options.UseSqlServer(connectionString));
+
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
